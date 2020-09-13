42b82a2fa7737817c973df06fe78d8ab434f63db
Bakhtiar <bakhtiarkamangar@gmail.com>
2020-09-13 11:36:21 +0430

    commit 23.6.99

--- config/database.php
+++ config/database.php
@@ -48,7 +48,7 @@ return [
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => "poorya",
+            'database' => "poorya_develop",
             'username' => "root",
             'password' => "697444",
             'unix_socket' => env('DB_SOCKET', ''),
